89b145a83b9248b8bbf3b1987cacc8de8ff0b0a3
KewneWhimsy <josshanel@gmail.com>
2024-11-18 19:20:41 +0100

    popup event

--- index.js
+++ index.js
@@ -115,8 +115,8 @@ app.get("/api/event/:id", async (req, res) => {
     const event = result.rows[0];
 
     const eventHtml = `
-      <div class="bg-white p-6 rounded-lg shadow-lg max-w-md mx-auto">
-        <h2 class="text-2xl font-bold mb-4">${event.title}</h2>
+      <div class="bg-[#26232A] border p-6 rounded-lg shadow-lg max-w-md mx-auto">
+        <h2 class="text-2xl font-bold mb-4 font-heading text-heading truncate-2-lines">${event.title}</h2>
         <p class="mb-4">${event.description}</p>
         <p><strong>Joueurs :</strong> ${event.players_count}</p>
         <p><strong>Organisateur :</strong> ${event.organisateur}</p>
