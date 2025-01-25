92845a6e5ee95019313015cb6a12afa12c1d20c5
KewneWhimsy <josshanel@gmail.com>
2025-01-25 03:09:30 +0100

    update events3

--- src/controllers/eventsController.js
+++ src/controllers/eventsController.js
@@ -441,7 +441,7 @@ module.exports.myEventById = async (req, res) => {
 module.exports.updateEvent = async (req, res) => {
   console.log("POST updateEvent");
   const { title, description, players_count, start_datetime, end_datetime } = req.body;
-  const { userId, role } = req.user;
+  const userId = req.user.userId;
   const eventId = req.params.eventId;
 
   // Validation des données
