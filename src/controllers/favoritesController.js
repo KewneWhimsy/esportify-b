3b9dc8265aa809290d92bb9fc3d4887c74175592
KewneWhimsy <josshanel@gmail.com>
2025-01-29 01:52:41 +0100

    room event20

--- src/controllers/favoritesController.js
+++ src/controllers/favoritesController.js
@@ -39,36 +39,21 @@ module.exports.toggleFavorite = async (req, res) => {
   // Génération du bouton mis à jour
 
   const buttonHtml = isFavoritedBool
-    ? `<div x-show="favorite">
-          <!-- Bouton pour retirer des favoris -->
-          <button
-            x-show="!ongoing"
-            hx-post="https://esportify-backend.onrender.com/api/favorites"
-            hx-target="#favorite-button"
-            hx-vals='${JSON.stringify({
-              event_id: event_id,
-              user_id: user_id,
-              isFavorited: false,
-            })}'
-            hx-headers='{"Content-Type": "application/json"}'
-            hx-encoding="json"
-            hx-swap="innerHTML"
-            class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
-          >
-            Plus intéressé
-          </button>
-          <!-- Bouton Rejoindre -->
-          <button
-            x-show="ongoing"
-            id="boutonRejoindre"
-            hx-get="https://esportify-backend.onrender.com/api/room/${event_id}"
-            hx-target="body"
-            hx-push-url="/event/${event_id}/room"
-            class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
-          >
-            Rejoindre
-          </button>
-        </div>`
+    ? `<button
+           hx-post="https://esportify-backend.onrender.com/api/favorites"
+           hx-target="#favorite-button"
+           hx-vals='${JSON.stringify({
+             event_id: event_id,
+             user_id: user_id,
+             isFavorited: false,
+           })}'
+          hx-headers='{"Content-Type": "application/json"}'
+          hx-encoding="json"
+           hx-swap="innerHTML"
+           class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
+         >
+           Plus intéressé
+         </button>`
     : `<button
            hx-post="https://esportify-backend.onrender.com/api/favorites"
            hx-target="#favorite-button"
