32e1f24dc99a6f812ed9e2788dc3c9eb9055f09b
KewneWhimsy <josshanel@gmail.com>
2025-01-29 03:15:46 +0100

    event favoris >=now

--- src/controllers/favoritesController.js
+++ src/controllers/favoritesController.js
@@ -117,6 +117,7 @@ module.exports.showFavorited = async (req, res) => {
       JOIN users u ON e.user_id = u.id
       JOIN favorites f ON f.event_id = e.id
       WHERE f.user_id = $1
+      AND e.start_datetime >= NOW()
       ORDER BY ${sortColumn} ASC
       `,
       [userId]
