0d766c30c6e1ac6cfa6d1468ebc7b67b85cae17b
KewneWhimsy <josshanel@gmail.com>
2025-01-29 02:57:10 +0100

    test

--- src/controllers/favoritesController.js
+++ src/controllers/favoritesController.js
@@ -56,9 +56,9 @@ module.exports.toggleFavorite = async (req, res) => {
   // Génération du bouton mis à jour
 
   const buttonHtml = isFavoritedBool
-    ? `<div x-data='${JSON.stringify({
-              ongoing: isOngoing,
-            })}' x-show="favorite">
+    ? `<div x-data="{
+        ongoing: ${isOngoing}
+      }" x-show="favorite">
           <!-- Bouton pour retirer des favoris -->
           <button
             x-show="!ongoing"
